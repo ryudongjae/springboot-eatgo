9734969d6059bbef85e2a4b0fc2de78cacb88bda
ryudongjae <rdj10149@gmail.com>
2021-01-25 00:25:28 +0900

    FirstCommit

---
+++ src/main/java/kr/co/project/eat/EatApplication.java
@@ -0,0 +1,13 @@
+package kr.co.project.eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/project/eat/EatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.project.eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
