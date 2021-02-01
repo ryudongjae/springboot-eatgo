b190b0f371a42a289d2a5c699961676e49374ad4
ryudongjae <rdj10149@gmail.com>
2021-02-01 14:04:16 +0900

    Update

--- eatgo-api/src/main/java/kr/co/project/eat/interfaces/WelcomeController.java
+++ eatgo-api/src/main/java/kr/co/project/eat/interfaces/WelcomeController.java
@@ -3,7 +3,7 @@ package kr.co.project.eat.interfaces;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController// Json 형태로 객체 데이터를 반환
 public class WelcomeController {
     @GetMapping("/")
     public String hello(){
