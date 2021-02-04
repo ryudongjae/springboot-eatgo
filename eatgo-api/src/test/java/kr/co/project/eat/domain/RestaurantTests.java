06935ef0ae3fc993a90a4e78d01dee163c06f441
ryudongjae <rdj10149@gmail.com>
2021-02-04 11:44:31 +0900

    Update

--- eatgo-api/src/test/java/kr/co/project/eat/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/project/eat/domain/RestaurantTests.java
@@ -15,6 +15,24 @@ public class RestaurantTests {
         assertThat(restaurant.getAddress(), is("Seoul"));
     }
     @Test
+    public void information1(){
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+    @Test
+    public void information2(){
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+    @Test
+    public void information3(){
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+    @Test
     public void information(){
         Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
 
